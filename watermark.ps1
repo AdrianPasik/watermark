009c8c458270a072625cdd3365263ac22713db1b
unknown <adrian@nonexistendmail.com>
2019-09-03 00:39:40 +0200

    working with starting process

--- watermark.ps1
+++ watermark.ps1
@@ -1,3 +1,12 @@
 $workdir="C:\test_rysunkow";
 $targetDir="C:\test_rysunkow\output";
+$magickExeFile = "C:\Program Files\ImageMagick-7.0.8-Q16\magick.exe";
 
+$files = Get-ChildItem -File $workdir;
+
+foreach($file in $files) {
+    # $command = "$magickExeFile convert $workdir\$file -resize 50% $targetDir\$file"
+    # & $command;
+    Start-Process -NoNewWindow -FilePath $magickExeFile -ArgumentList "convert $workdir\$file -resize 50% $targetDir\$file"
+
+}
