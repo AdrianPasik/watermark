daf755d6fd1f879b35fb57a68a9d8d72e6f1fb27
unknown <adrian@nonexistendmail.com>
2019-09-03 23:48:26 +0200

    working watermark but ugly

--- watermark.ps1
+++ watermark.ps1
@@ -2,11 +2,16 @@ $workdir="C:\test_rysunkow";
 $targetDir="C:\test_rysunkow\output";
 $magickExeFile = "C:\Program Files\ImageMagick-7.0.8-Q16\magick.exe";
 
-$files = Get-ChildItem -File $workdir;
+#$arguments = "convert $workdir\watermark.png +profile \""icc\"" $workdir\tmp.png"
+#Start-Process -NoNewWindow -FilePath $magickExeFile -ArgumentList "$arguments"
 
+$files = Get-ChildItem -File $workdir;
 foreach($file in $files) {
     # $command = "$magickExeFile convert $workdir\$file -resize 50% $targetDir\$file"
     # & $command;
-    Start-Process -NoNewWindow -FilePath $magickExeFile -ArgumentList "convert $workdir\$file -resize 50% $targetDir\$file"
-
+    # Start-Process -NoNewWindow -FilePath $magickExeFile -ArgumentList "convert $workdir\$file -resize 50% $targetDir\$file"ccc
+    #$arguments = "convert $workdir\$file -background transparent -fill grey -font Calibri -pointsize 14 -gravity center caption:\""Dom\n sadf\"" $targetDir\$file"
+    
+    $arguments = "composite -gravity center $workdir\watermark.png $workdir\$file $targetDir\$file"
+    Start-Process -NoNewWindow -FilePath $magickExeFile -ArgumentList "$arguments"
 }
